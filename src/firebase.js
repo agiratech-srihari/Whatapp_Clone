cf44092b799551a7ca99e17307f7ffd00ab83527
agiratech-srihari <srihari.t@agiratech.com>
2022-12-13 19:56:36 +0530

    working on whatsapp web

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCTapkMxFtZW8FkflYft0iKC3f68lLuBX0",
-  authDomain: "whatapp-clone-124f9.firebaseapp.com",
-  projectId: "whatapp-clone-124f9",
-  storageBucket: "whatapp-clone-124f9.appspot.com",
-  messagingSenderId: "486513615122",
-  appId: "1:486513615122:web:ab1934a18535315ce713cb",
-};
+const fire = process.env.firebaseConfig;
